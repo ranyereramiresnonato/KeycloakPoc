a4a33fa657f38cc934626a27083da5f337933a78
ranyereramiresdeveloper <ranyereramiresdeveloper@hotmail.com>
2025-11-10 09:59:22 -0300

    Iniciando a construção da api

---
+++ PurchasesApi/Controllers/PurchasesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PurchasesApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PurchasesController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Acesso autorizado! Token válido do Keycloak." });
+        }
+
+        [HttpGet("admin")]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult GetAdmin()
+        {
+            return Ok(new { message = "Acesso liberado para Administrator!" });
+        }
+    }
+}
--- PurchasesApi/Program.cs
+++ PurchasesApi/Program.cs
@@ -1,13 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = "http://localhost:8080/realms/fintech-poc"; // realm URL
+    options.Audience = "purchases-api"; // client id
+    options.RequireHttpsMetadata = false; // desativa HTTPS só para dev
+
+    // Validação de token (opcional mas recomendado)
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
